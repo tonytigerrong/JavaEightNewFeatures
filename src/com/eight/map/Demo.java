cadc9004cd93eb0079ec4d996f1c6de6fb15b0d8
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-06-18 15:18:04 -0300

    map ; flatmap

--- src/com/eight/map/Demo.java
+++ src/com/eight/map/Demo.java
@@ -1,9 +1,36 @@
 package com.eight.map;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Demo {
 
 	
 	public static void main(String[] args){
+		
+		System.out.println("General map Example");
+		List<Integer> intList = Arrays.asList(1,2,3,4,5);
+		intList = intList.stream().map(i->i*2).collect(Collectors.toList());
+		intList.forEach(System.out::print);
+		
+		System.out.println();
+		System.out.println("Flat map Example");
+		String[][] data = new String[][]{{"a","b"},{"c","e"},{"f","g"}};
+		Stream<String[]> stream = Arrays.stream(data);
+		stream.forEach(strList->{
+			for(String str : strList)
+				System.out.print(str);
+		});
+		System.out.println();
+		stream = Arrays.stream(data);
+		Stream<String> stream2 = stream.flatMap(strs->Arrays.stream(strs));
+		stream2.forEach(System.out::print);
+		
+		
+		
 		
 	}
 }
